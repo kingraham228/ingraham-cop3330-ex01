11734af6399b4291ee7e63c926ba622b8476585f
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-05-27 17:00:52 -0400

    Full separation of function.

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -3,16 +3,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Name userName = new Name();
-        Greeting userGreeting = new Greeting();
-        Compliment userCompliment = new Compliment();
-
-        constructGreeting(userName, userGreeting, userCompliment);
+        System.out.printf("%s",constructGreeting(new Name(), new Greeting(), new Compliment()) );
 
     }
 
-    public static void constructGreeting(Name n, Greeting g, Compliment c){
-        System.out.printf("%s%s%s",g.chooseGreeting(), n.getName(), c.chooseCompliment());
+    public static String constructGreeting(Name n, Greeting g, Compliment c){
+        return g.chooseGreeting() + n.getName() + c.chooseCompliment();
 
     }
+
+
 }
